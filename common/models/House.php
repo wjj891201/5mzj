80c92231db722c36ea21fe67352d928964ba1f82
wujiepeng <517987404@qq.com>
2019-05-14 18:00:10 +0800

    二手房编辑

--- common/models/House.php
+++ common/models/House.php
@@ -58,7 +58,7 @@ class House extends ActiveRecord
     }
 
     /**
-     * 添加方法
+     * 二手房添加方法
      * @param type $data
      * @return boolean
      */
@@ -84,6 +84,24 @@ class House extends ActiveRecord
                 $temp[$key] = ['tab_id' => $house_sales_id, 'obj_lab' => $vo, 'cre_time' => date('Y-m-d H:i:s')];
             }
             Yii::$app->db->createCommand()->batchInsert("{{%obj_lab}}", ['tab_id', 'obj_lab'], $temp)->execute();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 二手房编辑方法
+     * @param type $data
+     * @return boolean
+     */
+    public function edit($data)
+    {
+        if ($this->load($data) && $this->save())
+        {
+            $house_id = $this->id;
+            # 房源出售信息
+            $is_recomm = self::handRecomm($this->recommend, $this->high_quality);
+            Yii::$app->db->createCommand()->update("{{%house_sales}}", ['is_mortgage' => $this->is_mortgage, 'price1' => $this->price1, 'to_price1' => $this->to_price1, 'is_recomm' => $is_recomm, 'user_grade' => $this->user_grade, 'mod_time' => date('Y-m-d H:i:s')], ['house_id' => $house_id])->execute();
             return true;
         }
         return false;
