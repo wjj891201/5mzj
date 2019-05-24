93912610a14fc1bb955a505040e8d3612dbf6801
wujiepeng <517987404@qq.com>
2019-05-24 17:52:13 +0800

    角色管理

--- backend/views/role/list.php
+++ backend/views/role/list.php
@@ -13,11 +13,7 @@ use yii\helpers\Url;
 <div class="page-container">
     <div class="cl pd-5 bg-1 bk-gray">
         <span class="l"> 
-            <a href="javascript:;" onclick="datadel()" class="btn btn-danger radius">
-                <i class="Hui-iconfont">&#xe6e2;</i> 
-                批量删除
-            </a>
-            <a class="btn btn-primary radius" href="javascript:;" onclick="operate_full('添加角色', '<?= Url::to(['role/add']) ?>')">
+            <a class="btn btn-primary radius" href="<?= Url::to(['role/add']) ?>">
                 <i class="Hui-iconfont">&#xe600;</i> 
                 添加角色
             </a> 
@@ -30,7 +26,6 @@ use yii\helpers\Url;
                 <th scope="col" colspan="6">角色管理</th>
             </tr>
             <tr class="text-c">
-                <th width="25"><input type="checkbox" value="" name=""></th>
                 <th width="40">ID</th>
                 <th width="200">角色名</th>
                 <th>用户列表</th>
@@ -40,36 +35,11 @@ use yii\helpers\Url;
         </thead>
         <tbody>
             <tr class="text-c">
-                <td><input type="checkbox" value="" name=""></td>
                 <td>1</td>
                 <td>超级管理员</td>
                 <td><a href="#">admin</a></td>
                 <td>拥有至高无上的权利</td>
                 <td class="f-14"><a title="编辑" href="javascript:;" onclick="admin_role_edit('角色编辑', 'admin-role-add.html', '1')" style="text-decoration:none"><i class="Hui-iconfont">&#xe6df;</i></a> <a title="删除" href="javascript:;" onclick="admin_role_del(this, '1')" class="ml-5" style="text-decoration:none"><i class="Hui-iconfont">&#xe6e2;</i></a></td>
-            </tr>
-            <tr class="text-c">
-                <td><input type="checkbox" value="" name=""></td>
-                <td>2</td>
-                <td>总编</td>
-                <td><a href="#">张三</a></td>
-                <td>具有添加、审核、发布、删除内容的权限</td>
-                <td class="f-14"><a title="编辑" href="javascript:;" onclick="admin_role_edit('角色编辑', 'admin-role-add.html', '2')" style="text-decoration:none"><i class="Hui-iconfont">&#xe6df;</i></a> <a title="删除" href="javascript:;" onclick="admin_role_del(this, '1')" class="ml-5" style="text-decoration:none"><i class="Hui-iconfont">&#xe6e2;</i></a></td>
-            </tr>
-            <tr class="text-c">
-                <td><input type="checkbox" value="" name=""></td>
-                <td>3</td>
-                <td>栏目主辑</td>
-                <td><a href="#">李四</a>，<a href="#">王五</a></td>
-                <td>只对所在栏目具有添加、审核、发布、删除内容的权限</td>
-                <td class="f-14"><a title="编辑" href="javascript:;" onclick="admin_role_edit('角色编辑', 'admin-role-add.html', '3')" style="text-decoration:none"><i class="Hui-iconfont">&#xe6df;</i></a> <a title="删除" href="javascript:;" onclick="admin_role_del(this, '1')" class="ml-5" style="text-decoration:none"><i class="Hui-iconfont">&#xe6e2;</i></a></td>
-            </tr>
-            <tr class="text-c">
-                <td><input type="checkbox" value="" name=""></td>
-                <td>4</td>
-                <td>栏目编辑</td>
-                <td><a href="#">赵六</a>，<a href="#">钱七</a></td>
-                <td>只对所在栏目具有添加、删除草稿等权利。</td>
-                <td class="f-14"><a title="编辑" href="javascript:;" onclick="admin_role_edit('角色编辑', 'admin-role-add.html', '4')" style="text-decoration:none"><i class="Hui-iconfont">&#xe6df;</i></a> <a title="删除" href="javascript:;" onclick="admin_role_del(this, '1')" class="ml-5" style="text-decoration:none"><i class="Hui-iconfont">&#xe6e2;</i></a></td>
             </tr>
         </tbody>
     </table>
