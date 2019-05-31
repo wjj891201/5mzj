d2aad35de1a2ff72a939a013bdbd13c15fe2a9b5
wujiepeng <517987404@qq.com>
2019-05-31 16:10:27 +0800

    gitignore

---
+++ vendor/filsh/yii2-oauth2-server/storage/Pdo.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace filsh\yii2\oauth2server\storage;
+
+class Pdo extends \OAuth2\Storage\Pdo
+{
+
+    public $dsn;
+    public $username;
+    public $password;
+    public $connection = 'db';
+
+    public function __construct($connection = null, $config = array())
+    {
+        if ($connection === null)
+        {
+            if (!empty($this->connection))
+            {
+                $connection = \Yii::$app->get($this->connection);
+                if (!$connection->getIsActive())
+                {
+                    $connection->open();
+                }
+                $connection = $connection->pdo;
+            }
+            else
+            {
+                $connection = [
+                    'dsn' => $this->dsn,
+                    'username' => $this->username,
+                    'password' => $this->password
+                ];
+            }
+        }
+
+        parent::__construct($connection, $config);
+        $this->config = array_merge(
+                [
+            'client_table' => \Yii::$app->db->tablePrefix . 'oauth_clients',
+            'access_token_table' => \Yii::$app->db->tablePrefix . 'oauth_access_tokens',
+            'refresh_token_table' => \Yii::$app->db->tablePrefix . 'oauth_refresh_tokens',
+            'code_table' => \Yii::$app->db->tablePrefix . 'oauth_authorization_codes',
+            'user_table' => \Yii::$app->db->tablePrefix . 'oauth_users',
+            'jwt_table' => \Yii::$app->db->tablePrefix . 'oauth_jwt',
+//            'jti_table'           => \Yii::$app->db->tablePrefix . 'oauth_jti',
+            'scope_table' => \Yii::$app->db->tablePrefix . 'oauth_scopes',
+            'public_key_table' => \Yii::$app->db->tablePrefix . 'oauth_public_keys',
+                ], $config
+        );
+    }
+
+}
---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,115 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '2.0.7.0',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '2.0.8.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap/src',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '2.0.13.0',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '2.0.4.0',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+  'filsh/yii2-oauth2-server' => 
+  array (
+    'name' => 'filsh/yii2-oauth2-server',
+    'version' => '2.0.1.0',
+    'alias' => 
+    array (
+      '@filsh/yii2/oauth2server' => $vendorDir . '/filsh/yii2-oauth2-server',
+    ),
+    'bootstrap' => 'filsh\\yii2\\oauth2server\\Bootstrap',
+  ),
+  'rmrevin/yii2-fontawesome' => 
+  array (
+    'name' => 'rmrevin/yii2-fontawesome',
+    'version' => '2.17.1.0',
+    'alias' => 
+    array (
+      '@rmrevin/yii/fontawesome' => $vendorDir . '/rmrevin/yii2-fontawesome',
+    ),
+  ),
+  'cebe/yii2-gravatar' => 
+  array (
+    'name' => 'cebe/yii2-gravatar',
+    'version' => '1.1.0.0',
+    'alias' => 
+    array (
+      '@cebe/gravatar' => $vendorDir . '/cebe/yii2-gravatar/cebe/gravatar',
+    ),
+  ),
+  'dmstr/yii2-adminlte-asset' => 
+  array (
+    'name' => 'dmstr/yii2-adminlte-asset',
+    'version' => '2.6.0.0',
+    'alias' => 
+    array (
+      '@dmstr' => $vendorDir . '/dmstr/yii2-adminlte-asset',
+    ),
+  ),
+  'mdmsoft/yii2-admin' => 
+  array (
+    'name' => 'mdmsoft/yii2-admin',
+    'version' => '2.8.0.0',
+    'alias' => 
+    array (
+      '@mdm/admin' => $vendorDir . '/mdmsoft/yii2-admin',
+    ),
+  ),
+  'yiisoft/yii2-httpclient' => 
+  array (
+    'name' => 'yiisoft/yii2-httpclient',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/httpclient' => $vendorDir . '/yiisoft/yii2-httpclient/src',
+    ),
+  ),
+  'yiisoft/yii2-authclient' => 
+  array (
+    'name' => 'yiisoft/yii2-authclient',
+    'version' => '2.1.5.0',
+    'alias' => 
+    array (
+      '@yii/authclient' => $vendorDir . '/yiisoft/yii2-authclient/src',
+    ),
+  ),
+);
